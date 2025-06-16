94cfa8369a28d953a6ee5e86e724b52101dfe2bb
yusufhamzah-TIB <yusuf.hamzah.taufiqurrahman.tik24@stu.pnj.ac.id>
2025-06-16 15:38:58 +0700

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+    output: 'export',
+    basePath: '/tugasbesar2',
+    assetPrefix: '/tugasbesar2/',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
